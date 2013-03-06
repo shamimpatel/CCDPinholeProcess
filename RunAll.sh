253c1ea368f9852c8cdf6d2536891081ce4375b6
Shamim Patel <shamim.patel@gmail.com>
2013-03-06 09:19:40 +0000

    Added wait commands to plotting script

--- RunAll.sh
+++ RunAll.sh
@@ -1,5 +1,8 @@
 ./CCDPinholeProcessMain || exit 1
+sleep 1s
 ./PlotCCD.sh
 python ProduceSpectrum.py DiffractResultsPostPinhole.txt || exit 1
+sleep 1s
 ./PlotSpectrum.sh
+sleep 1s
 ./CorrelationPlot.sh
