dfb5bf3bf3d5ca03bd44cf568cd1fdc943627f2d
Shamim Patel <shamim.patel@gmail.com>
2013-03-12 12:22:22 +0000

    Producespectrum after binning to pixels

--- RunAll.sh
+++ RunAll.sh
@@ -1,7 +1,8 @@
 ./CCDPinholeProcessMain || exit 1
 sleep 0.5s
 ./PlotCCD.sh
-python ProduceSpectrum.py DiffractResultsPostPinhole.txt || exit 1
+#python ProduceSpectrum.py DiffractResultsPostPinhole.txt || exit 1
+python ProduceBinnedSpectrum.py
 sleep 0.5s
 ./PlotSpectrum.sh
 sleep 0.5s
